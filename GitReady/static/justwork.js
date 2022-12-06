28322486cac88a92ecea06e32d256e10845dccf9
BACGFI <47774380+KERMITSKILL@users.noreply.github.com>
2022-12-06 17:04:21 +0100

    Delete GitReady directory

--- GitReady/static/justwork.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('start').onclick = () =>{
-    var kod = document.getElementById('code').value;
-    console.log(kod.length);
-    if(kod.length >= 6){
-        chrome.runtime.sendMessage({'inject': kod.toString()});
-    }
-}
